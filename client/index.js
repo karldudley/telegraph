049f62b33aab1bb41e52a23986b5e95fc7d80457
mike-rv <mikerv73@gmail.com>
2022-09-14 12:16:20 +0100

    modify index.js add form name action tags

--- client/index.js
+++ client/index.js
@@ -9,26 +9,26 @@ runTest();
 
 const formData = document.querySelector('#form-data')
 
-form.addEventListener('submit', submitForm);
+// form.addEventListener('submit', submitForm);
 
-function submitForm(e) {
-    e.preventDefault();
+// function submitForm(e) {
+//     e.preventDefault();
 
-    const formData = {
-        name: e.target.name.value,
-        age: e.target.age.value,
-        title: e.target.name.value
-    };
+//     const formData = {
+//         name: e.target.name.value,
+//         age: e.target.age.value,
+//         title: e.target.name.value
+//     };
 
-    const options = {
-        method: 'POST',
-        body: JSON.stringify(formData),
-        headers: { "Content-Type": "application/json" }
-    };
+//     const options = {
+//         method: 'POST',
+//         body: JSON.stringify(formData),
+//         headers: { "Content-Type": "application/json" }
+//     };
 
-    // fetch('http://localhost:3000/dogs', options)
-    //     .then(r => r.json())
-    //     .then(appendDog)
-    //     .then(() => e.target.reset())
-    //     .catch(console.warn)
-};
+//     fetch('http://localhost:3000/add-telegraph/', options)
+//         .then(r => r.json())
+//         // .then(appendDog)
+//         .then(() => e.target.reset())
+//         .catch(console.warn)
+// };
